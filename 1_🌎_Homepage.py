aeed58e08dad160a11aab3e0808cfeeae6288514
Dylan <dylanzjames14@gmail.com>
2023-02-26 19:57:11 -0600

    Update 1_🌎_Homepage.py

--- 1_🌎_Homepage.py
+++ 1_🌎_Homepage.py
@@ -6,5 +6,5 @@ st.set_page_config(layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Free geospatial FMIS data generation.")
 
-m = leafmap.Map(center=[36.1627, -97.0872], zoom=15, tiles="dark", attr="Mapbox Attribution")
+m = leafmap.Map(google_map="hybrid",center=[36.1627, -97.0872], zoom=15,)
 m.to_streamlit(height=700)
