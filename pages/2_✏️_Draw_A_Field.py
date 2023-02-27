dda04c0b017933d7c2c1c670906cb68d2200d011
Dylan <dylanzjames14@gmail.com>
2023-02-26 20:06:38 -0600

    Update 2_✏️_Draw_A_Field.py

--- pages/2_✏️_Draw_A_Field.py
+++ pages/2_✏️_Draw_A_Field.py
@@ -6,7 +6,9 @@ import leafmap.foliumap as leafmap
 st.title("Create A Field")
 st.write("Use the map tools to draw your field.")
 st.write("Once complete, you can save it as a .shp, .geojson, or .kml")
-st.button("Save .shp")
+st.button("Save .shp") 
+st.button("Save .geojson") 
+st.button("Save .kml")
 
 m = leafmap.Map(google_map="hybrid",center=[36.1627, -97.0872], zoom=15,)
 m.to_streamlit(height=700)
