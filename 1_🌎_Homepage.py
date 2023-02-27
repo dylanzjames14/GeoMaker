02fb1e03cf0c8c01d5b1c1dadbe07ed1f4c8d424
Dylan <dylanzjames14@gmail.com>
2023-02-26 19:33:06 -0600

    Update 1_🌎_Homepage.py

--- 1_🌎_Homepage.py
+++ 1_🌎_Homepage.py
@@ -1,10 +1,10 @@
 import streamlit as st
+import folium
 import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Free geospatial FMIS data generation.")
 
-m = leafmap.Map(center=[40, -100], zoom=7, tiles="mapboxsatellite", attr="Mapbox Attribution")
+m = leafmap.Map(center=[36.1627, -97.0872], zoom=15, tiles="dark", attr="Mapbox Attribution")
 m.to_streamlit(height=700)
-
