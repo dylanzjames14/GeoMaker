7fabef1cb596895b0ac275cdf1b5f1b7e02b56cb
Dylan <dylanzjames14@gmail.com>
2023-03-01 22:31:59 -0600

    Update 3_📍_Create_Grid_Sampling.py

--- pages/3_📍_Create_Grid_Sampling.py
+++ pages/3_📍_Create_Grid_Sampling.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import geopandas as gpd
+import json
+import tempfile
+from zipfile import ZipFile
+from streamlit_folium import folium_static
+import folium
+import leafmap.foliumap as leafmap
+from folium.plugins import Draw
 
 st.title("Create Grid Sampling")
-st.write("This tool will allow you to configure your grid to your field and save it as a shapefile.")
+st.write("This tool will allow you to configure a sampling scheme for a provided field and save it as a shapefile which may be imported in to Sirrus!")
 
+st.file_uploader("Upload your .shp, .geojson, or .kml",accept_multiple_files=False, type=['shp','geojson','kml'])
+m = leafmap.Map(google_map="hybrid",center=[36.1256, -97.0665], zoom=10,draw_control=False,measure_control=False, fullscreen_control=False)
+draw_control = Draw(export=False,filename="boundary.geojson", draw_options={"polygon":False,"circle":False,"rectangle":False ,"polyline":False, "circlemarker":False})
+draw_control.add_to(m)
+
+m.to_streamlit(height=600)
