51e23a97d4bcdf14794976da0022044872390f31
Dylan <dylanzjames14@gmail.com>
2023-03-02 07:32:26 -0600

    Update 3_✏️_Draw_A_Field.py

--- pages/3_✏️_Draw_A_Field.py
+++ pages/3_✏️_Draw_A_Field.py
@@ -9,9 +9,9 @@ import leafmap.foliumap as leafmap
 from folium.plugins import Draw
 
 st.title("Draw A Field")
-st.write("1. Use the map tools to locate the area of interest. You may pan or use the search function.")
-st.write("2. Once you've located your field, use the draw tools to draw your field boundary.")
-st.write("3. Click *Export*. This will save a .geojson file which may be imported into Sirrus!")
+st.write("1. Use the map tools to locate your field. *You may pan or use the search function*.")
+st.write("2. Once you've located your field, use the draw tools to draw around your field.")
+st.markdown("3. Click *Export*. This will save a .geojson file which may be imported into Sirrus!")
 
 m = leafmap.Map(google_map="hybrid",center=[36.1256, -97.0665], zoom=10,draw_control=False,measure_control=False, fullscreen_control=False)
 draw_control = Draw(export=True,filename="boundary.geojson", draw_options={"marker":False, "polyline":False, "circlemarker":False})
