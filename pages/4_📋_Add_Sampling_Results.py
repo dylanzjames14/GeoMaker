a6c9e625ad7b43d81501f336c35607e485a9f934
Dylan <dylanzjames14@gmail.com>
2023-03-01 23:04:49 -0600

    Update 4_📋_Add_Sampling_Results.py

--- pages/4_📋_Add_Sampling_Results.py
+++ pages/4_📋_Add_Sampling_Results.py
@@ -7,7 +7,7 @@ st.write("Create a Modus results XML file with specified sample IDs")
 
 
 # Use the "../" notation to navigate to the parent directory
-file_path = "https://github.com/dylanzjames14/GeoMaker/blob/17b1a69be76074f6f376eac579ed4a4d8d338574/Data/Mock%20Soil%20Test%20Data.xlsx"
+file_path = "Data/Mock Soil Test Data.xlsx"
 
 # Read the Excel file into a pandas DataFrame
 df = pd.read_excel(file_path)
