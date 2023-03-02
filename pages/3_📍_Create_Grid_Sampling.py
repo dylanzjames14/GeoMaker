c238624653d1b31ff9389d5e3ac34bea1db3a199
Dylan <dylanzjames14@gmail.com>
2023-03-01 22:46:29 -0600

    Update 3_📍_Create_Grid_Sampling.py

--- pages/3_📍_Create_Grid_Sampling.py
+++ pages/3_📍_Create_Grid_Sampling.py
@@ -11,7 +11,7 @@ from folium.plugins import Draw
 
 st.info("This app is under construction, but have feel free to have a play with the map!")
 st.title("Create Grid Sampling")
-st.write("This tool will allow you to configure a sampling scheme for a provided field and save it as a shapefile which may be imported in to Sirrus!")
+st.write("This tool will allow you to configure a sampling scheme and save it as a shapefile.")
 
 st.file_uploader("Upload your .shp, .geojson, or .kml",accept_multiple_files=False, type=['shp','geojson','kml'])
 m = leafmap.Map(google_map="hybrid",center=[36.1256, -97.0665], zoom=10,draw_control=False,measure_control=False, fullscreen_control=False)
