40b0e859bb13bfff24da6defc213fb32ae28cb1e
Dylan <dylanzjames14@gmail.com>
2023-03-03 17:51:09 -0600

    Update 5_📋_Add_Sampling_Results.py

--- pages/5_📋_Add_Sampling_Results.py
+++ pages/5_📋_Add_Sampling_Results.py
@@ -5,16 +5,16 @@ st.info("This app is under construction, but enjoy reviewing the randomized samp
 st.title("Add Sampling Results")
 
 # Radio button to select between LBS and PPM
-unit = st.radio("Select the unit of measurement", ('LBS', 'PPM'))
+unit = st.radio("Select units", ('LBS', 'PPM'))
 
 # Slider to select the range of rows to display
-num_rows = st.slider("Select the number of rows to display", 1, 100)
+num_rows = st.slider("Select the number samples", 1, 100)
 
 # CSV file path based on the selected unit of measurement
 if unit == 'LBS':
-    file_path = "Data/sample_data_lbs.csv"
+    file_path = "Data/lbs.csv"
 else:
-    file_path = "Data/sample_data_ppm.csv"
+    file_path = "Data/ppm.csv"
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(file_path, nrows=num_rows+1)
