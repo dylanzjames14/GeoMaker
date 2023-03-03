f4409faa6ad13bab5b3954e8b155ce0182dd4184
Dylan <dylanzjames14@gmail.com>
2023-03-03 17:38:41 -0600

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -1,18 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-# Add your Google Analytics tracking ID here
-GA_TRACKING_ID = 'G-XSVGC64PMC'
-
-# Define a function that sends a pageview to Google Analytics
-def track_pageview(page):
-    if GA_TRACKING_ID:
-        pageview_url = f'https://www.google-analytics.com/collect?v=1&t=pageview&tid={GA_TRACKING_ID}&cid=CLIENT_ID_NUMBER_HERE&dp={page}'
-        st.write(pageview_url)
-
-# Call the track_pageview function with the current page name
-track_pageview('/home')
-
 st.set_page_config(layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Geomaker is a tool for generating precision ag data! The goal of this site is to make it simple to produce test data anywhere on the planet.")
