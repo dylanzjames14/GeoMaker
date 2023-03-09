3d8ca91481328bc888468237134e36f41c667ca3
Dylan <dylanzjames14@gmail.com>
2023-03-09 07:57:49 -0600

    Update 5_📋_Add_Sampling_Results.py

--- pages/5_📋_Add_Sampling_Results.py
+++ pages/5_📋_Add_Sampling_Results.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 st.info("This app is under construction.")
@@ -14,8 +15,17 @@ with col1:
 with col2:
     depth_unit = st.radio("Depth units:", ("Inches", "Centimeters"))
 
+# Load soil test data
+if unit == 'LBS':
+    soil_test_data = pd.read_excel("Data/lbsSoilTest.xlsx")
+else:
+    soil_test_data = pd.read_excel("Data/ppmSoilTest.xlsx")
+
 # Range sliders for setting min and max range of sample IDs
 min_sample_id, max_sample_id = st.slider("Set SampleID range", 1, 500, (1, 500), 1)
+
+# Filter soil test data based on SampleID range
+soil_test_data = soil_test_data[(soil_test_data.SampleID >= min_sample_id) & (soil_test_data.SampleID <= max_sample_id)]
 
 # Dropdown to select the number of depths sampled
 num_depths = st.selectbox("Unique depths collected at each sample", [0, 1, 2, 3, 4, 5], key='num_depths', index=0)
@@ -51,15 +61,13 @@ for i in range(num_depths + 1):
         })
 
 
-# Display the resulting DataFrame if the form has been submitted
+# Display filtered soil test data, hiding columns with missing values
 st.write("---")
-st.write("Sample Results")
-st.write("min_sample_id:", min_sample_id)
-st.write("max_sample_id:", max_sample_id)
-st.write("unit:", unit)
-st.write("depth_unit:", depth_unit)
-st.write("num_depths:", num_depths)
-st.write("max_depths:", max_depths[1:])
+st.write("Soil Test Data")
+filtered_soil_test_data = soil_test_data[(soil_test_data['SampleID'] >= min_sample_id) & (soil_test_data['SampleID'] <= max_sample_id)]
+filtered_soil_test_data = filtered_soil_test_data.drop(columns=['ID'], errors='ignore')  # Drop the 'ID' column if it exists
+filtered_soil_test_data = filtered_soil_test_data.dropna(axis=1, how='all')  # Drop columns with all missing values
+st.dataframe(filtered_soil_test_data)
 
 # Display depth references
 st.write("---")
@@ -77,3 +85,5 @@ for i, depth_ref in enumerate(depth_refs):
              f"    <DepthUnit>{depth_ref['DepthUnit']}</DepthUnit>\n"
              f"  </DepthRef>\n"
              f"</DepthRefs>\n")
+
+
