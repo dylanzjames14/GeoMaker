288da6cc89489b05d27d022f04835883043d1a10
Dylan <dylanzjames14@gmail.com>
2023-03-26 08:46:20 -0500

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -7,9 +7,14 @@ st.write("Geomaker is a tool for generating precision ag application data and in
 
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
+    st.subheader("California Central Valley")
     st.image("Data/RockwoodCalifornia.gif")
-    st.image("Data/SWKansasPivots.gif")
+    st.subheader("Boothill Missouri")
+    st.image("Data/BoothillMissouri.gif")
+    
 
 with row1_col2:
-    st.image("Data/BoothillMissouri.gif")
+    st.subheader("Mississippi River Valley")
     st.image("Data/MississippiRiverValley.gif")
+    st.subheader("Ogallala Aquifer")
+    st.image("Data/SWKansasPivots.gif")
