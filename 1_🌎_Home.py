ed1350360adb970a2fde24082eec475ea4189766
Dylan <dylanzjames14@gmail.com>
2023-03-26 07:34:23 -0500

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -8,7 +8,7 @@ st.write("Geomaker is a tool for generating precision ag application data and in
 row1_col1, row1_col2 = st.columns(2)
 with row1_col1:
     st.image("Data/RockwoodCalifornia.gif")
-    st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
+    st.image("Data/SWKansasPivots.gif")
 
 with row1_col2:
     st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
