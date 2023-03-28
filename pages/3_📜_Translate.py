48104b15e883352f57b3584a93ac86d1b09adb94
Dylan <dylanzjames14@gmail.com>
2023-03-28 14:15:26 -0500

    Update 3_📜_Translate.py

--- pages/3_📜_Translate.py
+++ pages/3_📜_Translate.py
@@ -4,7 +4,7 @@ import openai
 st.set_page_config(layout="wide")
 st.title("📜 Translate")
 st.write("The tool is designed to translate agricultural application text strings like a professional in the field. You can choose the language you want to translate into and input the text to be translated.")
-st.warning("Information translated using this tool is being translated by OpenAI's davinci-003 model. Please be careful what you input as it is being monitored by OpenAI.")
+st.warning("Information translated using this tool is being translated via OpenAIs API.")
 
 openai.api_key = st.secrets["api_secret"]
 
