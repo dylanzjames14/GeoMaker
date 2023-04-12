79f861b52776e6694226b3057a3dda8a6d39b988
Dylan James <dylanzjames14@gmail.com>
2023-04-12 06:58:46 -0500

    Update 7_📍_Create_Sampling_Points.py

--- pages/7_📍_Create_Sampling_Points.py
+++ pages/7_📍_Create_Sampling_Points.py
@@ -1,21 +1,73 @@
+import folium
 import streamlit as st
-import geopandas as gpd
 import json
 import tempfile
-from zipfile import ZipFile
-from streamlit_folium import folium_static
-import folium
-import leafmap.foliumap as leafmap
 from folium.plugins import Draw
+from streamlit_folium import st_folium
+from zipfile import ZipFile
+from io import BytesIO
+import fiona
+import os
+from shapely.geometry import Point, mapping
 
-st.set_page_config(layout="wide")
-st.info("This app is under construction, but have a play with the map!")
+def save_geojson_to_shapefile(all_drawings, filename):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Define schema
+        schema = {
+            'geometry': 'Point',
+            'properties': [('Name', 'str'), ('SampleID', 'int')]
+        }
+
+        # Open a Fiona object
+        shapefile_filepath = os.path.join(tmpdir, f"{filename}.shp")
+        with fiona.open(shapefile_filepath, mode='w', driver='ESRI Shapefile', schema=schema, crs="EPSG:4326") as shp_file:
+            for idx, feature in enumerate(all_drawings):
+                if feature['geometry']['type'] == 'Point':
+                    point = Point(feature['geometry']['coordinates'])
+                    shape = {
+                        'geometry': mapping(point),
+                        'properties': {'Name': f"Sample {idx}", 'SampleID': idx + 1}
+                    }
+                    shp_file.write(shape)
+
+        # Create a zip file containing the shapefile components
+        with BytesIO() as buffer:
+            with ZipFile(buffer, "w") as zip_file:
+                for extension in ["shp", "shx", "dbf", "prj"]:
+                    zip_file.write(os.path.join(tmpdir, f"{filename}.{extension}"), f"SamplePoints.{extension}")
+            buffer.seek(0)
+            return buffer.read()
+
 st.title("📍 Create Sampling Points")
 st.write("This tool will allow you to configure a sampling scheme and save it as a shapefile.")
 
-st.file_uploader("Upload your .shp, .geojson, or .kml",accept_multiple_files=False, type=['shp','geojson','kml'])
-m = leafmap.Map(google_map="hybrid",center=[36.1256, -97.0665], zoom=10,draw_control=False,measure_control=False, fullscreen_control=False)
-draw_control = Draw(export=False,filename="boundary.geojson", draw_options={"polygon":False,"circle":False,"rectangle":False ,"polyline":False, "circlemarker":False})
+m = folium.Map(
+    location=[36.1256, -97.0665],
+    zoom_start=10,
+    tiles="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+    attr="Google"
+)
+
+# Customize the options for the Draw plugin
+draw_options = {
+    "position": "topleft",
+    "polyline": False,
+    "circle": False,
+    "rectangle": False,
+    "polygon": False,
+    "circlemarker": False,
+    "marker": True,
+}
+
+draw_control = Draw(export=False, draw_options=draw_options)
 draw_control.add_to(m)
 
-m.to_streamlit(height=600)
+# Display the map without columns
+returned_objects = st_folium(m, width=1000, height=600, returned_objects=["all_drawings"])
+
+if st.button("Save to Shapefile"):
+    if isinstance(returned_objects, dict) and 'all_drawings' in returned_objects and len(returned_objects['all_drawings']) > 0:
+        shapefile_data = save_geojson_to_shapefile(returned_objects['all_drawings'], "SamplePoints")
+        st.download_button("Download Shapefile", shapefile_data, "GeoMaker_Point_Shapefile.zip", "application/zip")
+    else:
+        st.warning("No markers found. Please add markers to the map.")
