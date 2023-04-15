d2c4e3f33d4317a4e73f3588bd22eca401254ff9
Dylan James <dylanzjames14@gmail.com>
2023-04-15 08:13:53 -0500

    Update 4_✏️_Draw_A_Field.py

--- pages/4_✏️_Draw_A_Field.py
+++ pages/4_✏️_Draw_A_Field.py
@@ -1,27 +1,3 @@
-import streamlit as st
-import geopandas as gpd
-import json
-import tempfile
-from zipfile import ZipFile
-from streamlit_folium import folium_static
-import folium
-import leafmap.foliumap as leafmap
-from folium.plugins import Draw
-
-st.set_page_config(layout="wide")
-st.title("✏️ Draw A Field")
-st.markdown("""
-1. Use the map tools to locate the area of interest. You may pan or use the search function.
-2. Once you've located your field, use the draw tools to draw your field boundary.
-3. Click **Export**. 
-""")
-
-
-m = leafmap.Map(google_map="hybrid",center=[36.1256, -97.0665], zoom=10,draw_control=False,measure_control=False, fullscreen_control=False)
-draw_control = Draw(export=True,filename="boundary.geojson", draw_options={"marker":False, "polyline":False, "circlemarker":False})
-draw_control.add_to(m)
-
-m.to_streamlit(height=700)
 import folium
 import streamlit as st
 import json
@@ -99,5 +75,3 @@ if st.button("Save to Shapefile"):
         st.download_button("Download Shapefile", shapefile_data, "Drawn_Polygons_Shapefile.zip", "application/zip")
     else:
         st.warning("No polygons found. Please draw polygons on the map.")
-
-
