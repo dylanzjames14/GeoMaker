9c86686c27f48c46c075faf42cb4c24a19b717c7
Dylan James <dylanzjames14@gmail.com>
2023-04-16 07:16:59 -0500

    Adding KML & SHP save options

--- pages/4_✏️_Draw_A_Field.py
+++ pages/4_✏️_Draw_A_Field.py
@@ -9,6 +9,7 @@ from io import BytesIO
 import fiona
 import os
 from shapely.geometry import shape as shapely_shape, mapping
+import simplekml
 
 def save_geojson_to_shapefile(all_drawings, filename):
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -38,11 +39,26 @@ def save_geojson_to_shapefile(all_drawings, filename):
             buffer.seek(0)
             return buffer.read()
 
+def save_geojson_to_kml(all_drawings, filename):
+    kml = simplekml.Kml()
+    for idx, feature in enumerate(all_drawings):
+        if feature['geometry']['type'] == 'Polygon':
+            polygon = shapely_shape(feature['geometry'])
+            kml.newpolygon(name=f"Polygon {idx}", outerboundaryis=list(polygon.exterior.coords))
+    return kml.kml()
+
+
 st.title("✏️ Draw a Field")
 st.markdown("""
 1. Navigate to your field on the map and draw a field boundary using the map tools.
-2. Once complete, click **Save to Shapefile** and download your resulting .zip containing your polygons.
+2. Once complete, click the appropriate button to save your drawn polygons.
 """)
+
+# Save buttons in columns
+col1, col2, col3 = st.columns(3)
+save_shapefile_button = col1.button("Save to Shapefile")
+save_kml_button = col2.button("Save KML")
+save_geojson_button = col3.button("Save GEOJSON")
 
 m = folium.Map(
     location=[36.1256, -97.0665],
@@ -55,7 +71,7 @@ m = folium.Map(
 draw_options = {
     "position": "topleft",
     "polyline": False,
-    "circle": True,
+    "circle": False,
     "rectangle": True,
     "polygon": True,
     "circlemarker": False,
@@ -66,11 +82,26 @@ draw_control = Draw(export=False, draw_options=draw_options)
 draw_control.add_to(m)
 
 # Display the map without columns
+
 returned_objects = st_folium(m, width=1000, height=550, returned_objects=["all_drawings"])
 
-if st.button("Save to Shapefile"):
+if save_shapefile_button:
     if isinstance(returned_objects, dict) and 'all_drawings' in returned_objects and len(returned_objects['all_drawings']) > 0:
         shapefile_data = save_geojson_to_shapefile(returned_objects['all_drawings'], "DrawnPolygons")
-        st.download_button("Download Shapefile", shapefile_data, "Drawn_Polygons_Shapefile.zip", "application/zip")
+        col1.download_button("Download Shapefile", shapefile_data, "Drawn_Polygons_Shapefile.zip", "application/zip")
     else:
-        st.warning("No polygons found. Please draw polygons on the map.")
+        col1.warning("No polygons found. Please draw polygons on the map.")
+
+if save_kml_button:
+    if isinstance(returned_objects, dict) and 'all_drawings' in returned_objects and len(returned_objects['all_drawings']) > 0:
+        kml_data = save_geojson_to_kml(returned_objects['all_drawings'], "DrawnPolygons")
+        col2.download_button("Download KML", kml_data, "Drawn_Polygons.kml", "application/vnd.google-earth.kml+xml")
+    else:
+        col2.warning("No polygons found. Please draw polygons on the map.")
+
+if save_geojson_button:
+    if isinstance(returned_objects, dict) and 'all_drawings' in returned_objects and len(returned_objects['all_drawings']) > 0:
+        geojson_data = json.dumps({"type": "FeatureCollection", "features": returned_objects['all_drawings']})
+        col3.download_button("Download GEOJSON", geojson_data, "Drawn_Polygons.geojson", "application/geo+json")
+    else:
+        col3.warning("No polygons found. Please draw polygons on the map.")
