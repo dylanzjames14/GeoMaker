bd4f70c128248aeeb30eaed4fe7cc594a1ca7c1c
Dylan James <dylanzjames14@gmail.com>
2023-04-25 07:06:32 -0500

    Update 7_📋_Make_Modus_Sampling_Results.py
    Moved validator into the modus creation tool

--- pages/7_📋_Make_Modus_Sampling_Results.py
+++ pages/7_📋_Make_Modus_Sampling_Results.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import datetime
 import base64
+from lxml import etree
+from io import StringIO, BytesIO
+import os
 
 st.set_page_config(layout="wide")
 st.title("📋 Make Modus Sampling Results")
@@ -279,7 +282,7 @@ else:
     data = st.session_state.data
 
 # Add a button to generate random values
-if st.button("Generate Soil Test Data"):
+if st.button("Generate Random Values"):
     for column in data.columns:
         if column != 'SampleNumber':  # Skip the 'SampleNumber' column
             min_value, max_value = default_min_max_values[column]
@@ -471,3 +474,47 @@ if xml_strings:
     b64 = base64.b64encode(xml_strings.encode()).decode()
     href = f'<a href="data:file/xml;base64,{b64}" download="{filename}">Download Modus XML File</a>'
     st.markdown(href, unsafe_allow_html=True)
+
+
+# Create the expander
+xml_validator_expander = st.expander("✅ Modus file validator", expanded=False)
+
+# Place the entire code block inside the expander
+with xml_validator_expander:
+    class SchemaEntityResolver(etree.Resolver):
+        def resolve(self, url, pubid, context):
+            if 'modus_global.xsd' in url:
+                return self.resolve_filename(os.path.join('Data', 'modus_global.xsd'), context)
+            elif 'modus_submit.xsd' in url:
+                return self.resolve_filename(os.path.join('Data', 'modus_submit.xsd'), context)
+            return self.resolve_filename(url, context)
+
+    def validate_xml(xml_data, schema_path):
+        try:
+            parser = etree.XMLParser(load_dtd=True, no_network=False)
+            parser.resolvers.add(SchemaEntityResolver())
+            schema_doc = etree.parse(schema_path, parser)
+            schema = etree.XMLSchema(schema_doc)
+
+            xml_parser = etree.XMLParser(schema=schema)
+            etree.parse(BytesIO(xml_data), xml_parser)
+            return True, None
+        except etree.XMLSchemaError as e:
+            return False, str(e)
+        except etree.XMLSyntaxError as e:
+            return False, str(e)
+
+    # Upload the XML file
+    uploaded_file = st.file_uploader("", type="xml")
+    if uploaded_file is not None:
+        xml_data = uploaded_file.read()
+
+        # Set the schema file path
+        schema_path = os.path.join("Data", "modus_result.xsd")
+
+        is_valid, error_message = validate_xml(xml_data, schema_path)
+
+        if is_valid:
+            st.success("The XML file is valid.")
+        else:
+            st.error(f"Invalid XML file. Error message: {error_message}")
