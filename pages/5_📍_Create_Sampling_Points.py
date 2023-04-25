5ae6c37f9b981ef64afc6ae74d7bf0b7928743a9
Dylan James <dylanzjames14@gmail.com>
2023-04-24 19:29:48 -0500

    Update 5_📍_Create_Sampling_Points.py

--- pages/5_📍_Create_Sampling_Points.py
+++ pages/5_📍_Create_Sampling_Points.py
@@ -9,6 +9,7 @@ from io import BytesIO
 import fiona
 import os
 from shapely.geometry import Point, mapping
+from shapely.geometry import shape as shapely_shape, MultiPolygon
 
 st.set_page_config(layout="wide")
 
@@ -41,17 +42,47 @@ def save_geojson_to_shapefile(all_drawings, filename):
             return buffer.read()
 
 st.title("📍 Create Sampling Points")
-st.markdown("""
-1. Find your field using the map and drop points using the **Point** tool on the map.
-2. Once complete, click **Save to Shapefile** and download your resulting .zip containing your points.
-""")
+
+# Create an expander for the instructions
+instructions_expander = st.expander("Instructions", expanded=False)
+with instructions_expander:
+    st.markdown("""
+    - Find your field using the map and drop points using the **Point** tool on the map.
+    - If you have saved a field boundary on the '✏️ Draw a field' page, it will be displayed on the map. The map will be centered and zoomed to the field's location.
+    - Once complete, click **Save to Shapefile** and download your resulting .zip containing your points.
+    """)
 
 # Create an empty placeholder for the buttons
 buttons_placeholder = st.empty()
 
+# Calculate the bounding box of the saved polygons
+def get_polygon_bounds(polygon_features):
+    polygons = [shapely_shape(feature['geometry']) for feature in polygon_features if feature['geometry']['type'] == 'Polygon']
+    if not polygons:
+        return None
+
+    multipolygon = MultiPolygon(polygons)
+    return multipolygon.bounds
+
+# Set the map's initial location and zoom level based on the saved polygons
+if 'saved_geography' in st.session_state:
+    bounds = get_polygon_bounds(st.session_state.saved_geography)
+    if bounds:
+        min_lon, min_lat, max_lon, max_lat = bounds
+        center_lat = (min_lat + max_lat) / 2
+        center_lon = (min_lon + max_lon) / 2
+        location = [center_lat, center_lon]
+        zoom_start = 15
+    else:
+        location = [36.1256, -97.0665]
+        zoom_start = 10
+else:
+    location = [36.1256, -97.0665]
+    zoom_start = 10
+
 m = folium.Map(
-    location=[36.1256, -97.0665],
-    zoom_start=10,
+    location=location,
+    zoom_start=zoom_start,
     tiles="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
     attr="Google"
 )
@@ -69,6 +100,19 @@ draw_options = {
 
 draw_control = Draw(export=False, draw_options=draw_options)
 draw_control.add_to(m)
+
+# Check if there is a polygon saved in the session state
+if 'saved_geography' in st.session_state:
+    # Add the saved polygons to the map
+    for feature in st.session_state.saved_geography:
+        if feature['geometry']['type'] == 'Polygon':
+            polygon = folium.GeoJson(
+                data=feature,
+                style_function=lambda x: {"fillColor": "blue", "color": "blue", "weight": 2},
+            )
+            polygon.add_to(m)
+else:
+    st.warning("No saved polygons found. Please draw and save a polygon on the other page.")
 
 # Display the map without columns
 returned_objects = st_folium(m, width=1000, height=550, returned_objects=["all_drawings"])
