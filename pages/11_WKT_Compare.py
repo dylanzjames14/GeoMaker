32a6cc1e5c181e29dd467a07ca7f5ba5d5f00743
Dylan James <dylanzjames14@gmail.com>
2023-07-14 14:52:10 -0500

    Update 11_WKT_Compare.py

--- pages/11_WKT_Compare.py
+++ pages/11_WKT_Compare.py
@@ -3,21 +3,36 @@ import geopandas as gpd
 import folium
 from shapely import wkt
 from streamlit_folium import folium_static
+from shapely.errors import WKTReadingError
 
 # Setup Streamlit layout
 st.set_page_config(layout="wide")
+st.title('Compare Two WKT Polygons')
+st.markdown("""
+    **Instructions:** Paste the Well-Known Text (WKT) for each of the polygons you want to compare in the boxes below.
+    The polygons will be displayed on a map, and you will receive statistics for each, including area, perimeter, and extent.
+""")
 
 # Create two columns for user input
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
 
 # Get WKT from user
-wkt1 = col1.text_input('Paste your first WKT here')
-wkt2 = col2.text_input('Paste your second WKT here')
+with col1:
+    st.markdown('**Polygon 1 (Red):**')
+    wkt1 = st.text_area('Paste your first WKT here')
+
+with col2:
+    st.markdown('**Polygon 2 (Blue):**')
+    wkt2 = st.text_area('Paste your second WKT here')
 
 # Create polygons from WKT and calculate stats
 if wkt1 and wkt2:
-    poly1 = wkt.loads(wkt1)
-    poly2 = wkt.loads(wkt2)
+    try:
+        poly1 = wkt.loads(wkt1)
+        poly2 = wkt.loads(wkt2)
+    except WKTReadingError:
+        st.error('Invalid WKT. Please check your inputs.')
+        return
 
     # Create a GeoSeries from the polygons
     gdf = gpd.GeoSeries([poly1, poly2])
@@ -40,19 +55,21 @@ if wkt1 and wkt2:
     folium_static(m)
 
     # Calculate stats for each polygon
-    col1.write('**Polygon 1 Stats:**')
-    col1.write('Area 1 (m²): ', poly1.area)
-    col1.write('Perimeter 1 (m): ', poly1.length)
-    col1.write('Bounds 1: ', str(poly1.bounds))
+    with col1:
+        st.subheader('Polygon 1 Stats:')
+        st.write('Area 1 (m²): {}'.format(poly1.area))
+        st.write('Perimeter 1 (m): {}'.format(poly1.length))
+        st.write('Bounds 1: {}'.format(poly1.bounds))
 
-    col2.write('**Polygon 2 Stats:**')
-    col2.write('Area 2 (m²): ', poly2.area)
-    col2.write('Perimeter 2 (m): ', poly2.length)
-    col2.write('Bounds 2: ', str(poly2.bounds))
+    with col2:
+        st.subheader('Polygon 2 Stats:')
+        st.write('Area 2 (m²): {}'.format(poly2.area))
+        st.write('Perimeter 2 (m): {}'.format(poly2.length))
+        st.write('Bounds 2: {}'.format(poly2.bounds))
 
     # Compare polygons' stats
     st.subheader('Comparison of polygons')
-    st.write('Difference in area (m²): ', abs(poly1.area - poly2.area))
-    st.write('Difference in perimeter (m): ', abs(poly1.length - poly2.length))
-    st.write('Difference in extent: ', abs(poly1.bounds[2] - poly2.bounds[2]), 'm in x direction, ',
-              abs(poly1.bounds[3] - poly2.bounds[3]), 'm in y direction')
+    st.write('Difference in area (m²): {}'.format(abs(poly1.area - poly2.area)))
+    st.write('Difference in perimeter (m): {}'.format(abs(poly1.length - poly2.length)))
+    st.write('Difference in extent: {} m in x direction, {} m in y direction'.format(
+        abs(poly1.bounds[2] - poly2.bounds[2]), abs(poly1.bounds[3] - poly2.bounds[3])))
