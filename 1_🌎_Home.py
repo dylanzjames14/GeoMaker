74dca6a080c8635d2d637ea52ef7fe377d608467
Dylan James <dylanzjames14@gmail.com>
2023-07-15 08:14:05 -0500

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+st.set_page_config(page_title="Geomaker", page_icon="🌍")
 st.set_page_config(layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Geomaker is a tool for generating precision ag application data and information!")
