5aab7c320b1506cce0c95fe72ebd93f08bb8f7c0
Dylan James <dylanzjames14@gmail.com>
2023-07-15 08:14:55 -0500

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.set_page_config(page_title="Geomaker", page_icon="🌍")
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Geomaker", page_icon="🌍", layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Geomaker is a tool for generating precision ag application data and information!")
 st.write("Also a tutorial of how to use this in conjunction with Sirrus can be found at: https://youtu.be/fDxPmvvSfM8")
