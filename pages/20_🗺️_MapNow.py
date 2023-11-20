582e6e4ff3807e9074336a0a2461bec6a8f15ca7
Dylan James <dylanzjames14@gmail.com>
2023-11-20 07:26:23 -0600

    Update 20_🗺️_MapNow.py
    UI Rework

--- pages/20_🗺️_MapNow.py
+++ pages/20_🗺️_MapNow.py
@@ -1,175 +1,208 @@
+from streamlit_folium import st_folium
 import folium
 import streamlit as st
 from folium.plugins import Draw
-from streamlit_folium import st_folium
-from shapely.geometry import shape, mapping
+from streamlit_folium import folium_static
+from shapely.geometry import shape
+from datetime import date
+import uuid
 
-if not st.session_state.get('initialized'):
-    st.session_state.drawn_geometries = []
-    st.session_state.drawn_markers = []
+def reset_session_state():
+    st.session_state.field_id = 1
+    st.session_state.label_id = 1
+    st.session_state.next_id = 0
+    st.session_state.field_maps = {}
+    st.session_state.marker_maps = {}
     st.session_state.legend_entries = {}
-    st.session_state.initialized = True
 
 st.set_page_config(layout="wide")
+
+# Assign keys to all buttons
+add_field_key = "add_field"
+add_label_key = "add_label"
+reset_top_key = "reset_top"
+reset_bottom_key = "reset_bottom"
+
+# Initialize session state for separate counters for fields and labels
+if 'initialized' not in st.session_state:
+    st.session_state.initialized = True
+if 'field_id' not in st.session_state:
+    st.session_state.field_id = 1
+if 'label_id' not in st.session_state:
+    st.session_state.label_id = 1
+
+# Initialize session state
+if 'next_id' not in st.session_state:
+    st.session_state.next_id = 0  # Unique ID generator
+if 'field_maps' not in st.session_state:
+    st.session_state.field_maps = {}
+if 'marker_maps' not in st.session_state:
+    st.session_state.marker_maps = {}
+if 'legend_entries' not in st.session_state:
+    st.session_state.legend_entries = {}
+
+
 st.title("MapNow 🗺️")
 st.warning("🚧 Work in Progress 🚧")
+st.markdown("""
+Welcome to **MapNow**! This interactive tool allows you to  add fields and markers on a map, customize their details, and generate MapNow report JSON.
+""")
 
-with st.expander("Instructions & Welcome Message", expanded=False):
-    st.write("""
-    Welcome to MapNow! 
-    Here, you can outline fields and place markers on the map, then specify details for each.
-    """)
-    st.markdown("### Instructions:")
+# Instructions in an Expander
+with st.expander("How to Use", expanded=False):
     st.markdown("""
-    1. **Draw a field boundary or place a marker** on the map.
-    2. Once drawn or placed, the boundary/marker will appear below the map with inputs to specify details.
-    3. Use the color picker to choose a color for the field boundary or marker.
-    4. For fields, provide details in the "Grower", "Farm", "Field", "Area", and "STR" inputs.
-    5. For markers, provide a label.
-    6. To clear all fields and markers and start fresh, click the "Clear all" button.
+    - **Add Fields**: Click on 'Add Field' to place a new field on the map. You can define its area, color, and other details.
+    - **Add Markers**: Use the 'Add Marker' button to drop markers on the map for specific locations.
+    - **Customize Legends**: Each field color can have a legend entry. Add a label to describe what each color represents.
+    - **Generate Report**: After setting up your fields and markers, the 'Generated JSON' section will display the report data in JSON format.
     """)
+    st.write("Feel free to explore and interact with the map. Please note that there may be some bugs as we continually work to improve this tool. Enjoy mapping!")
 
-location = [36.1256, -97.0665]
-m = folium.Map(
-    location=location,
-    zoom_start=11,
-    tiles="https://mt1.google.com/vt/lyrs=y@18&x={x}&y={y}&z={z}",
-    attr="Google"
-)
+# Modify the create_map function
+def create_map(map_type, key):
+    folium_map = folium.Map(
+        location=[36.1256, -97.0665],
+        zoom_start=11,
+        tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+        attr="Google"
+    )
+    
+    draw_options = {
+        "position": "topleft",
+        "polyline": False,
+        "circle": False,
+        "rectangle": True if map_type == 'field' else False,
+        "polygon": True if map_type == 'field' else False,
+        "circlemarker": False,
+        "marker": True if map_type == 'marker' else False  
+    }
 
-draw_options = {
-    "position": "topleft",
-    "polyline": False,
-    "circle": False,
-    "rectangle": True,
-    "polygon": True,
-    "circlemarker": False,
-    "marker": True  
-}
+    draw_control = Draw(draw_options=draw_options)
+    draw_control.add_to(folium_map)
+    return folium_map
 
-draw_control = Draw(draw_options=draw_options)
-draw_control.add_to(m)
-returned_objects = st_folium(m, width='100%', height=650)
+# Function to calculate the area of a polygon
+def calculate_area(polygon):
+    geom = shape(polygon)
+    area_acres = geom.area * (10**4) * 247.105
+    return area_acres
 
-if returned_objects and 'all_drawings' in returned_objects and returned_objects['all_drawings']:
-    for feature in returned_objects['all_drawings']:
-        if feature['geometry']['type'] == 'Polygon':
-            existing_geometries = [entry['geometry'] for entry in st.session_state.drawn_geometries if 'geometry' in entry]
-            if feature['geometry'] not in existing_geometries:
-                s = shape(feature['geometry'])
-                # Convert the area from square degrees to acres (assuming the map uses EPSG:4326 WGS 84)
-                # Note: This is an approximate conversion and might not be accurate for large areas.
-                area_acres = s.area * (10**4) * 247.105
-                st.session_state.drawn_geometries.append({
-                    'geometry': feature['geometry'],
-                    'color': '#FFFFFF',
-                    'grower': '',
-                    'farm': '',
-                    'field': '',
-                    'area': f"{area_acres:.2f} acres",
-                    'STR': ''
-                })
-        elif feature['geometry']['type'] == 'Point':
-            existing_markers = [entry['geometry'] for entry in st.session_state.drawn_markers if 'geometry' in entry]
-            if feature['geometry'] not in existing_markers:
-                st.session_state.drawn_markers.append({
-                    'geometry': feature['geometry'],
-                    'color': '#FFFFFF',
-                    'label': ''
-                })
+# Report Metadata
+st.subheader("Report Metadata")
+if st.button("Reset all", key=reset_top_key):
+    reset_session_state()
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    report_title = st.text_input("Report title:", key="report_title")
+with col2:
+    grower = st.text_input("Grower name:", key="grower")
+with col3:
+    printed_on = st.date_input("Printed on:", value=date.today(), key="printed_on")
+with col4:
+    printed_by = st.text_input("Printed by:", key="printed_by")
 
-if st.button("Reset all"):
-    st.session_state.drawn_geometries = []
-    st.session_state.drawn_markers = []
-
-st.markdown("---")
-st.subheader("Report Title")
-report_title = st.text_input("", value="Enter your report title here")
-
-st.markdown("---")
+# Section for Fields
+st.markdown("---")  
 st.subheader("Fields")
-fields_data = []
 
-for idx, geo_entry in enumerate(st.session_state.drawn_geometries):
-    if 'geometry' in geo_entry:
-        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-        with col1:
-            st.write(f"Field {idx+1}")
-        with col2:
-            geo_entry['color'] = st.color_picker("Pick a color", geo_entry.get('color', '#000000'), key=f"color_picker_{idx}")
-        with col3:
-            geo_entry['grower'] = st.text_input(f"Grower", geo_entry.get('grower', ''), key=f"grower_input_{idx}")
-        with col4:
-            geo_entry['farm'] = st.text_input(f"Farm", geo_entry.get('farm', ''), key=f"farm_input_{idx}")
-        with col5:
-            geo_entry['field'] = st.text_input(f"Field", geo_entry.get('field', ''), key=f"field_input_{idx}")
-        with col6:
-            geo_entry['area'] = st.text_input(f"Area", geo_entry.get('area', '0 acres'), key=f"area_input_{idx}")
-        with col7:
-            geo_entry['STR'] = st.text_input(f"STR", geo_entry.get('STR', ''), key=f"STR_input_{idx}")
+if st.button("Add Field", key=add_field_key):
+    field_key = f'field_{st.session_state.field_id}'
+    st.session_state.field_id += 1
+    new_map = create_map('field', field_key)
+    st.session_state.field_maps[field_key] = {'map': new_map, 'details': {'color': '#FFFFFF', 'area': '0 acres'}}
 
-        field_data = {
-            "geojson": {
-                "type": "Feature",
-                "properties": {},
-                "geometry": geo_entry['geometry']
-            },
-            "color": geo_entry['color'],
-            "area": geo_entry['area'],
-            "STR": geo_entry['STR'],
-            "labelLocation": f"{shape(geo_entry['geometry']).centroid.y},{shape(geo_entry['geometry']).centroid.x}"
-        }
-        fields_data.append(field_data)
+for key, value in st.session_state.field_maps.items():
+    if key.startswith("field_"):
+        with st.expander(f"Field {key.split('_')[1]}"):
+            cols = st.columns((1, 2, 2, 1))
 
-st.markdown("---")
-st.subheader("Map Labels")
+            color = cols[0].color_picker("Color", value['details']['color'], key=f"color_field_{key}")
+            farm = cols[1].text_input("Farm", key=f"farm_{key}")
+            field_name = cols[2].text_input("Field", key=f"field_{key}")
+            STR = cols[3].text_input("STR", key=f"STR_{key}")
 
-for idx, marker_entry in enumerate(st.session_state.drawn_markers):
-    if 'geometry' in marker_entry:
-        col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-        with col1:
-            st.write(f"Map Label {idx+1}")
-        with col2:
-            marker_entry['color'] = st.color_picker("Pick a color", marker_entry.get('color', '#000000'), key=f"marker_color_picker_{idx}")
-        with col3:
-            marker_entry['label'] = st.text_input(f"Label", marker_entry.get('label', ''), key=f"marker_label_input_{idx}")
+            # Folium map with st_folium
+            returned_objects = st_folium(value['map'], width=700, height=450)
 
-st.markdown("---")
+            if returned_objects and 'all_drawings' in returned_objects and returned_objects['all_drawings']:
+                for feature in returned_objects['all_drawings']:
+                    if feature['geometry']['type'] == 'Polygon':
+                        s = shape(feature['geometry'])
+                        area_acres = s.area * (10**4) * 247.105
+                        value['details']['area'] = f"{area_acres:.2f} acres"
+
+            cols[3].write(f"Area: {value['details']['area']}")
+
+            value['details'].update({
+                "color": color,
+                "farm": farm,
+                "field_name": field_name,
+                "STR": STR
+            })
+
+# Track unique colors
+unique_colors = set()
+for field_details in st.session_state.field_maps.values():
+    unique_colors.add(field_details['details']['color'])
+
+st.session_state.unique_colors = unique_colors
+
+
+# Section for Labels
+st.markdown("---")  
+st.subheader("Labels")
+if st.button("Add Label", key=add_label_key):
+    label_key = f'label_{st.session_state.label_id}'
+    st.session_state.label_id += 1
+    new_map = create_map('marker', label_key)
+    st.session_state.marker_maps[label_key] = {'map': new_map, 'details': {'color': '#FFFFFF'}}
+
+# Display each marker map
+for key, value in st.session_state.marker_maps.items():
+    if key.startswith("label_"):
+        with st.expander(f"Label {key.split('_')[1]}"):
+            cols = st.columns((1, 3))
+            color = cols[0].color_picker("Color", value['details']['color'], key=f"color_marker_{key}")
+            label = cols[1].text_input("Label", key=f"label_{key}")
+            folium_static(value['map'], width=700, height=450)
+
+st.markdown("---")  
 st.subheader("Legend")
 
-all_colors = set([geo_entry['color'] for geo_entry in st.session_state.drawn_geometries])
-all_colors.update([marker_entry['color'] for marker_entry in st.session_state.drawn_markers])
-
-for color in all_colors:
+# Iterate over unique colors and display legend entries
+for color in st.session_state.unique_colors:
+    # Display a color box next to the label input
+    st.markdown(f"<div style='display: inline-block; margin-right: 10px; vertical-align: middle;'><span style='display: inline-block; width: 25px; height: 25px; background-color: {color}; margin-right: 5px;'></span></div>", unsafe_allow_html=True)
+    
     if color not in st.session_state.legend_entries:
-        st.markdown(f"<div style='display: inline-block; margin-right: 10px; vertical-align: middle;'><span style='display: inline-block; width: 25px; height: 25px; background-color: {color}; margin-right: 5px;'></span></div>", unsafe_allow_html=True)
         description = st.text_input(f"Label for color {color}", key=f"legend_description_{color}")
         if description:
             st.session_state.legend_entries[color] = description
     else:
-        st.markdown(f"<div style='display: inline-block; margin-right: 10px;'><span style='display: inline-block; width: 25px; height: 25px; background-color: {color}; margin-right: 5px;'></span>{st.session_state.legend_entries[color]}</div>", unsafe_allow_html=True)
+        st.text_input(f"Label for color {color}", value=st.session_state.legend_entries[color], key=f"legend_description_{color}")
 
-st.markdown("---")
-st.subheader("Generated JSON")
+
+
+# Generate JSON
+fields_data = [value['details'] for value in st.session_state.field_maps.values()]
+markers_data = [value['details'] for value in st.session_state.marker_maps.values()]
 
 json_data = {
     "report": {
-        "title": report_title
+        "title": report_title,
+        "grower": grower,
+        "printed_on": printed_on.isoformat(),
+        "printed_by": printed_by
     },
     "map": {
-        "mapDefinition": {
-            "boundingBox": {
-                "topLeft": "40.812776,-74.105974",
-                "bottomRight": "40.512776,-73.805974"
-            },
-            "centroid": f"{location[0]},{location[1]}",
-            "zoomLevel": 12
-        },
-        "fields": fields_data
+        "fields": fields_data,
+        "markers": markers_data
     },
     "legend": {
         "colors": [{"description": st.session_state.legend_entries[color], "color": color} for color in st.session_state.legend_entries]
     }
 }
 
+st.markdown("---")  # Divider
+st.subheader("Generated JSON")
 st.json(json_data)
