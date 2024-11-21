3ecf758c9565b4a6adf3948bdfebf9780537300b
Dylan James <dylanzjames14@gmail.com>
2024-11-21 06:22:12 -0600

    Update 5_📍_Create_Sampling_Points.py

--- pages/5_📍_Create_Sampling_Points.py
+++ pages/5_📍_Create_Sampling_Points.py
@@ -1,48 +1,21 @@
-import folium
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
+from folium.plugins import Draw
+from shapely.geometry import Point, mapping, shape as shapely_shape, MultiPolygon
 import json
 import tempfile
-from folium.plugins import Draw
-from streamlit_folium import st_folium
-from zipfile import ZipFile
 from io import BytesIO
 import fiona
 import os
-from shapely.geometry import Point, mapping
-from shapely.geometry import shape as shapely_shape, MultiPolygon
-import geopandas as gpd
-from pykml import parser
-import requests
-from lxml import etree
 
-if 'uploaded_boundary' not in st.session_state:
-    st.session_state.uploaded_boundary = None
-
-if 'boundary_updated' not in st.session_state:
-    st.session_state.boundary_updated = False
+# Initialize session state variables
+if 'saved_geography' not in st.session_state:
+    st.session_state.saved_geography = []
 
 st.set_page_config(page_title="Geomaker", page_icon="🌍", layout="wide")
 
-buttons_placeholder2 = st.empty()
-
-def kml_to_geojson(kml_file):
-    with kml_file as f:
-        kml_str = f.read()
-    kml_doc = parser.fromstring(kml_str)
-    kml_str = etree.tostring(kml_doc, pretty_print=True).decode()
-    kml_str = kml_str.replace("gx:", "")
-    gdf = gpd.read_file(kml_str, driver='KML')
-    geojson = json.loads(gdf.to_json())
-    return geojson
-
-def shapefile_to_geojson(shp_file):
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with ZipFile(shp_file) as zip_file:
-            zip_file.extractall(tmpdir)
-        gdf = gpd.read_file(tmpdir)
-    geojson = json.loads(gdf.to_json())
-    return geojson
-
+# Function to save drawn points to a shapefile
 def save_geojson_to_shapefile(all_drawings, filename):
     with tempfile.TemporaryDirectory() as tmpdir:
         # Define schema
@@ -59,7 +32,7 @@ def save_geojson_to_shapefile(all_drawings, filename):
                     point = Point(feature['geometry']['coordinates'])
                     shape = {
                         'geometry': mapping(point),
-                        'properties': {'Name': f"Sample {idx}", 'SampleID': idx + 1}
+                        'properties': {'Name': f"Sample {idx + 1}", 'SampleID': idx + 1}
                     }
                     shp_file.write(shape)
 
@@ -67,15 +40,23 @@ def save_geojson_to_shapefile(all_drawings, filename):
         with BytesIO() as buffer:
             with ZipFile(buffer, "w") as zip_file:
                 for extension in ["shp", "shx", "dbf", "prj"]:
-                    zip_file.write(os.path.join(tmpdir, f"{filename}.{extension}"), f"SamplePoints.{extension}")
+                    zip_file.write(os.path.join(tmpdir, f"{filename}.{extension}"), f"{filename}.{extension}")
             buffer.seek(0)
             return buffer.read()
 
+# Function to get bounds of saved polygons
+def get_polygon_bounds(polygon_features):
+    polygons = [shapely_shape(feature['geometry']) for feature in polygon_features if feature['geometry']['type'] in ['Polygon', 'MultiPolygon']]
+    if not polygons:
+        return None
+    multipolygon = MultiPolygon(polygons)
+    return multipolygon.bounds
+
+# Application title
 st.title("📍 Create Sampling Points")
 
-# Create an expander for the instructions
-instructions_expander = st.expander("Click for instructions", expanded=False)
-with instructions_expander:
+# Instructions expander
+with st.expander("Click for instructions", expanded=False):
     st.markdown("""
     **Objective:** Drop sample points within your field.
 
@@ -83,61 +64,22 @@ with instructions_expander:
 
     1. **Find your field** on the map.
     2. **Drop points** using the **Point** tool on the map.
-    3. When finished, click **Save to Shapefile** and download your resulting .zip containing your points.
+    3. When finished, click **Save points to Shapefile** and download your resulting `.zip` containing your points.
+    """)
 
-    """, unsafe_allow_html=True)
-
-    #File Uploader
-    uploaded_file = st.file_uploader("Upload your zipped shapefile to set the frame to your boundary!", type=["zip"])
-    load_boundary_button = st.button("Load boundary to map")
-
-    if load_boundary_button:
-        if uploaded_file is not None:
-            file_content_type = uploaded_file.type
-            if file_content_type == "application/json":
-                st.session_state.uploaded_boundary = json.load(uploaded_file)
-            elif file_content_type == "application/vnd.google-earth.kml+xml":
-                st.session_state.uploaded_boundary = kml_to_geojson(uploaded_file)
-            elif file_content_type == "application/zip":
-                st.session_state.uploaded_boundary = shapefile_to_geojson(uploaded_file)
-            else:
-                st.error("Unsupported file format. Please upload a GeoJSON, KML, or Shapefile.")
-                
-            st.session_state.boundary_updated = True
-
-# Create a horizontal layout for the buttons
+# Buttons for actions
 button_col1, button_col2 = st.columns(2)
 with button_col1:
     save_to_shapefile_button = st.button("Save points to Shapefile")
 with button_col2:
-    remove_field_button = st.button("Remove field")
+    if st.session_state.saved_geography:
+        remove_field_button = st.button("Remove field")
+    else:
+        st.button("Remove field", disabled=True)
 
-# Calculate the bounding box of the saved polygons
-def get_polygon_bounds(polygon_features):
-    polygons = [shapely_shape(feature['geometry']) for feature in polygon_features if feature['geometry']['type'] == 'Polygon']
-    if not polygons:
-        return None
-
-    multipolygon = MultiPolygon(polygons)
-    return multipolygon.bounds
-
-def get_uploaded_boundary_bounds(uploaded_boundary):
-    if not uploaded_boundary:
-        return None
-
-    features = uploaded_boundary["features"]
-    polygons = [shapely_shape(feature['geometry']) for feature in features if feature['geometry']['type'] in ['Polygon', 'MultiPolygon']]
-    if not polygons:
-        return None
-
-    multipolygon = MultiPolygon(polygons)
-    return multipolygon.bounds
-
-# Set the map's initial location and zoom level based on the saved polygons or uploaded boundary
-if 'saved_geography' in st.session_state:
+# Determine map center and zoom
+if st.session_state.saved_geography:
     bounds = get_polygon_bounds(st.session_state.saved_geography)
-elif st.session_state.uploaded_boundary:
-    bounds = get_uploaded_boundary_bounds(st.session_state.uploaded_boundary)
 else:
     bounds = None
 
@@ -151,14 +93,15 @@ else:
     location = [36.1256, -97.0665]
     zoom_start = 10
 
+# Initialize map
 m = folium.Map(
     location=location,
     zoom_start=zoom_start,
-    tiles="https://mt1.google.com/vt/lyrs=y@18&x={x}&y={y}&z={z}",
+    tiles="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
     attr="Google"
 )
 
-# Customize the options for the Draw plugin
+# Add drawing control
 draw_options = {
     "position": "topleft",
     "polyline": False,
@@ -172,62 +115,26 @@ draw_options = {
 draw_control = Draw(export=False, draw_options=draw_options)
 draw_control.add_to(m)
 
-# Check if there is a polygon saved in the session state
-if 'saved_geography' in st.session_state:
-    # Add the saved polygons to the map
+# Add saved boundary to map
+if st.session_state.saved_geography:
     for feature in st.session_state.saved_geography:
-        if feature['geometry']['type'] == 'Polygon':
-            polygon = folium.GeoJson(
-                data=feature,
-                style_function=lambda x: {"fillColor": "blue", "color": "blue", "weight": 2},
-            )
-            polygon.add_to(m)
+        folium.GeoJson(
+            data=feature,
+            style_function=lambda x: {"fillColor": "blue", "color": "blue", "weight": 2, "fillOpacity": 0.3},
+        ).add_to(m)
 
-# Check if there is a polygons uploaded
-if uploaded_file is not None:
-    file_content_type = uploaded_file.type
-    if file_content_type == "application/json":
-        st.session_state.uploaded_boundary = json.load(uploaded_file)
-    elif file_content_type == "application/vnd.google-earth.kml+xml":
-        st.session_state.uploaded_boundary = kml_to_geojson(uploaded_file)
-    elif file_content_type == "application/zip":
-        st.session_state.uploaded_boundary = shapefile_to_geojson(uploaded_file)
-    else:
-        st.error("Unsupported file format. Please upload a GeoJSON, KML, or Shapefile.")
-    
-    st.session_state.boundary_updated = True
+# Display the map
+returned_objects = st_folium(m, width='100%', height=600, returned_objects=["all_drawings"])
 
-if st.session_state.uploaded_boundary and st.session_state.boundary_updated:
-    for feature in st.session_state.uploaded_boundary["features"]:
-        if feature["geometry"]["type"] == "Polygon" or feature["geometry"]["type"] == "MultiPolygon":
-            polygon = folium.GeoJson(
-                data=feature,
-                style_function=lambda x: {"fillColor": "blue", "color": "blue", "weight": 2},
-            )
-            polygon.add_to(m)
-    st.session_state.boundary_updated = False
-
-# Display the map without columns
-returned_objects = st_folium(m, width='100%', height=700, returned_objects=["all_drawings"])
-
-
-# Define the placeholder for the download button
-buttons_placeholder1 = st.empty()
-
+# Handle save to shapefile action
 if save_to_shapefile_button:
-    if returned_objects and isinstance(returned_objects, dict) and 'all_drawings' in returned_objects and len(returned_objects['all_drawings']) > 0:
+    if returned_objects and 'all_drawings' in returned_objects and returned_objects['all_drawings']:
         shapefile_data = save_geojson_to_shapefile(returned_objects['all_drawings'], "SamplePoints")
-        buttons_placeholder1.download_button("Download Shapefile", shapefile_data, "GeoMaker_Point_Shapefile.zip", "application/zip")
+        st.download_button("Download Shapefile", shapefile_data, "SamplePoints.zip", "application/zip")
     else:
-        st.warning("No markers found. Please add markers to the map.")
+        st.warning("No markers found. Please add markers to the map before saving.")
 
-
-if remove_field_button:
-    if 'saved_geography' in st.session_state:
-        del st.session_state.saved_geography
-        st.experimental_rerun()
-
-# Disable the 'Remove field' button if there is no field on the map
-if not ('saved_geography' in st.session_state and st.session_state.saved_geography):
-    remove_field_button = False
-    buttons_placeholder2.button("Remove field", disabled=True, key="remove_field_button_disabled")
+# Handle remove field action
+if 'remove_field_button' in locals() and remove_field_button:
+    st.session_state.saved_geography = []
+    st.experimental_rerun()
