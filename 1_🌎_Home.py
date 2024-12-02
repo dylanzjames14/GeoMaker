a4cebca8e2c5bba53b0ea5de105283bc9916ace7
Dylan James <dylanzjames14@gmail.com>
2024-12-02 10:28:25 -0600

    Update 1_🌎_Home.py

--- 1_🌎_Home.py
+++ 1_🌎_Home.py
@@ -5,16 +5,4 @@ st.set_page_config(page_title="Geomaker", page_icon="🌍", layout="wide")
 st.title("Welcome to GeoMaker!")
 st.write("Geomaker is a tool for generating precision ag application data and information!")
 
-row1_col1, row1_col2 = st.columns(2)
-with row1_col1:
-    st.subheader("California Central Valley")
-    st.image("Data/RockwoodCalifornia.gif")
-    st.subheader("Boothill Missouri")
-    st.image("Data/BoothillMissouri.gif")
-    
 
-with row1_col2:
-    st.subheader("Mississippi River Valley")
-    st.image("Data/MississippiRiverValley.gif")
-    st.subheader("Ogallala Aquifer")
-    st.image("Data/SWKansasPivots.gif")
